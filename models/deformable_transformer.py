23f31684a3ff31c9b9fef7024581fe169fbe86b7
Yun Jia <yunjia@me.com>
2024-05-31 15:04:42 -0700

    chore: update the module import

--- models/deformable_transformer.py
+++ models/deformable_transformer.py
@@ -21,7 +21,7 @@ from torch import nn, Tensor
 from torch.nn.init import xavier_uniform_, constant_, uniform_, normal_
 
 from util.misc import inverse_sigmoid
-from deformable_attention import MSDeformAttn
+from ops.modules import MSDeformAttn
 
 
 class DeformableTransformer(nn.Module):
